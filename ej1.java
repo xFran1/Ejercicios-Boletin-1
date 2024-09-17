b33e10315d6c9ce0090663069ff658091f6e3483
xFranvz <fran.rubio.ru@hotmail.com>
2024-09-17 21:15:34 +0200

    ej 1 hecho

--- ej1.java
+++ ej1.java
@@ -1,70 +1,118 @@
-
+import java.util.Scanner;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.lang.NumberFormatException;
 
 public class ej1 {
     public static void main(String[] args) {
-        
-        cambiaStrings("aeiou r", "eo z w");
-        //entreDosFechas( "05/06/2024", "25/06/2024");
-    
+        // cambiaStrings("aeiou r", "eo z w");
+
+        entreDosFechas();
+
     }
 
-    public static void cambiaStrings(String a,String b){
-
+    public static void cambiaStrings(String a, String b) {
+        System.out.println("entra");
         String primero = "";
         String segundo = "";
 
-        for (int i=0;i<a.length();i++) {
+        for (int i = 0; i < a.length(); i++) {
             boolean valido = true;
-            for (int j=0;j<b.length();j++) {
-                if(a.charAt(i)==b.charAt(j)){
-                    valido=false;
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    valido = false;
                 }
             }
-            if (valido==true) {
-                primero=primero+a.charAt(i);
+            if (valido == true) {
+                primero = primero + a.charAt(i);
             }
         }
 
-        for (int i=0;i<b.length();i++) {
+        for (int i = 0; i < b.length(); i++) {
             boolean valido = true;
-            for (int j=0;j<a.length();j++) {
-                if(b.charAt(i)==a.charAt(j)){
-                    valido=false;
+            for (int j = 0; j < a.length(); j++) {
+                if (b.charAt(i) == a.charAt(j)) {
+                    valido = false;
                 }
             }
-            if (valido==true) {
-                segundo=segundo+b.charAt(i);
+            if (valido == true) {
+                segundo = segundo + b.charAt(i);
             }
         }
-        
 
-        System.out.println(primero+" primero");
-        System.out.println(segundo+" segundo");
+        System.out.println(primero + " primero");
+        System.out.println(segundo + " segundo");
 
     }
 
-    public static void entreDosFechas(String a ,String b){
-        //Comprobamos el primer string
-        String[] separado1 = a.split("/");
-        String[] separado2 = b.split("/");
+    public static void entreDosFechas() {
+        Scanner s = new Scanner(System.in);
+        // Comprobamos el primer string
 
-        int primerNumero = Integer.parseInt(separado1[0]); 
-        int segundoNumero = Integer.parseInt(separado2[0]);
+        try {
 
-        primerNumero
+            System.out.println("Introduzca la primera fecha");
+            String a = s.next();
 
-        boolean valido1 = false;
-        boolean valido2 = false;
+            System.out.println("Introduzca la segunda fecha");
+            String b = s.next();
 
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-        for (int i = 1;i<31;i++) {
-            if ( primerNumero  ) {
-                
+            LocalDate fecha1 = LocalDate.parse(a, formatter);
+            LocalDate fecha2 = LocalDate.parse(b, formatter);
+
+            int dias = 0;
+
+            String fechaPrimera = "";
+            String fechaSegunda = "";
+
+            System.out.println("¿Cuál de las 2 fechas será la primera? (Elija 1 o 2)");
+            int answer = s.nextInt();
+
+            if (answer == 1) {
+                System.out.println("La primera fecha sera " + a);
+                System.out.println("La segunda fecha sera " + b);
+            } else {
+                System.out.println("La primera fecha sera " + b);
+                System.out.println("La segunda fecha sera " + a);
             }
-        }
-    }
 
+            if (fecha1.isBefore(fecha2)) { // Cual es mas vieja
+                fechaPrimera = b;
+                fechaSegunda = a;
+            } else if (fecha2.isBefore(fecha1)) {
+                fechaPrimera = a;
+                fechaSegunda = b;
+            }
+
+            String[] primera = fechaPrimera.split("/");
+            String[] segunda = fechaSegunda.split("/");
+
+            int primNum = Integer.parseInt(primera[0]);
+            int segNum = Integer.parseInt(segunda[0]);
+
+            dias = primNum - segNum; // Diferencia de dias
+
+            primNum = Integer.parseInt(primera[1]);
+            segNum = Integer.parseInt(segunda[1]);
+
+            dias = dias + ((primNum - segNum) * 30); // Diferencia de meses
+
+            primNum = Integer.parseInt(primera[2]);
+            segNum = Integer.parseInt(segunda[2]);
+
+            dias = dias + ((primNum - segNum) * 365); // Diferencia de años
+
+            System.out.println(dias);
+
+        } catch (DateTimeParseException e) {
+            System.out.println("Error: Formato de fecha inválido. Debe ser dd/MM/yyyy");
+
+        }
+
+    }
 
 }
-
